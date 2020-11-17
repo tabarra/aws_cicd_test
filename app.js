03c57302a9d04156d460c121f0d51f90af1dc0de
Andre Tabarra <tabarra@vexguard.net>
2020-11-17 00:47:56 -0300

    test beanstalk configs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello world from this deployment!')
+  res.send(`Hello world from this deployment: ${process.env.DUDE123}\t${process.env.AWS_REGION}`);
 })
 
 app.listen(port, () => {
